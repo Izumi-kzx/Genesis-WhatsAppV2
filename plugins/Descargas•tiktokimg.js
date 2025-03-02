ef22a44930215be5aaf1a52a6b26d6247d4aacaa
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-02 09:04:12 -0400

    Actualizar Descargas•tiktokimg.js

--- plugins/Descargas•tiktokimg.js
+++ plugins/Descargas•tiktokimg.js
@@ -1,28 +1,57 @@
-import Starlights from '@StarlightsTeam/Scraper'
+import axios from 'axios';
 
-let handler = async (m, { conn, usedPrefix, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, `[ ✰ ]  Ingresa un link de tiktok que contenga *imagenes*`, m, rcanal)
-if (!args[0].match(/tiktok/gi)) return conn.reply(m.chat, `[ ✰ ]  Verifica que el link sea de *TikTok*`, m, rcanal)
-await m.react('🕓')
-try {
-let { username, views, comments, shares, downloads, profile, dl_urls } = await Starlights.tiktokdlV2(args[0])
-let txt = '`乂  T I K T O K - I M G`\n\n'
-    txt += `\t\t*» Usuario* : ${username}\n`
-    txt += `\t\t*» Visitas* : ${views}\n`
-    txt += `\t\t*» Comentarios* : ${comments}\n`
-    txt += `\t\t*» Compartidos* : ${shares}\n`
-    txt += `\t\t*» Descargas* : ${downloads}\n`
-       
-for (let i = 0; i < dl_urls.length; i++) {
-await conn.sendFile(m.chat, dl_urls[i].dl_url, `tiktokimg${i + 1}.jpg`, txt, m)
-await m.react('✅')
-}} catch {
-await m.react('✖️')
-}}
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+    if (!args[0]) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `🍟 Ingresa un link de Instagram`, m, fake);
+    }
 
-handler.help = ['tiktokimg *<url tt>*']
-handler.tags = ['downloader']
-handler.command = ['tiktokimg', 'tiktokimgs', 'ttimg', 'ttimgs']
-handler.register = true
+    if (!args[0].match(new RegExp('^https?:\\/\\/www\\.instagram\\.com\\/([a-zA-Z0-9_-]+)\\/.*$'))) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `☁️ Verifica que sea un link válido de Instagram`, m);
+    }
 
-export default handler
+    try {
+        await m.react('🕑');
+        let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`);
+
+        let processedUrls = new Set();
+
+        for (let a of api.data.data) {
+            if (!processedUrls.has(a.url)) {
+                processedUrls.add(a.url);
+
+                // Verificación de si la URL es una imagen o un video
+                if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
+                    await conn.sendMessage(
+                        m.chat,
+                        { 
+                            image: { url: a.url }, 
+                            caption: '*✔️🍟Downloader instagram.*' 
+                        },
+                        { quoted: m }
+                    );
+                } else {
+                    await conn.sendMessage(
+                        m.chat,
+                        { 
+                            video: { url: a.url }, 
+                            caption: '*✔️🍟Downloader instagram.*' 
+                        },
+                        { quoted: m }
+                    );
+                }
+            }
+        }
+        await m.react('✅'); 
+    } catch (error) {
+        console.log(error);
+        await m.react('❌');
+    }
+};
+
+handler.help = ['ig *<link>*'];
+handler.tags = ['dl'];
+handler.command = /^(ig|igdl|instagram)$/i;
+
+export default handler;
