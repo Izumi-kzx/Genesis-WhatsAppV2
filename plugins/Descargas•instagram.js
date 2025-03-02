32afb3551bd91f79a8284563618f2f9a821f7999
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-02 08:48:06 -0400

    Actualizar Descargas•instagram.js

--- plugins/Descargas•instagram.js
+++ plugins/Descargas•instagram.js
@@ -1,24 +1,62 @@
-import fetch from 'node-fetch'
+/* 
+- Downloader Instagram By Izumi-kzx
+- Descarga todo de Instagram como publicaciones, historias, reels, destacadas
+- https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
+*/
+import axios from 'axios';
 
-let handler = async (m, { conn, usedPrefix, command, args }) => {
-if (!args[0]) return m.reply(`Ingresa un enlace de Instagram`)
-    
-try {
-let api = await fetch(`https://deliriussapi-oficial.vercel.app/download/instagram?url=${args[0]}`)
-let json = await api.json()
-let { data } = json
-let JT = data
-for (let i = 0; i < JT.length; i++) {
-let HFC = JT[i];
-if (HFC.type === "image") {
-await conn.sendMessage(m.chat, { image: { url: HFC.url } }, { quoted: m })
-} else if (HFC.type === "video") {
-await conn.sendMessage(m.chat, { video: { url: HFC.url } }, { quoted: m })
-}}
-} catch (error) {
-console.error(error)
-}}
+let handler = async (m, { conn, usedPrefix, args, command, text }) => {
+    if (!args[0]) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `🍟 Ingresa un link de Instagram`, m, fake);
+    }
 
-handler.command = /^(igdl|ig|instagramdl|instagram)$/i
+    if (!args[0].match(new RegExp('^https?:\\/\\/www\\.instagram\\.com\\/([a-zA-Z0-9_-]+)\\/.*$'))) {
+        await m.react('✖️');
+        return conn.reply(m.chat, `☁️ Verifica que sea un link válido de Instagram`, m);
+    }
 
-export default handler
+    try {
+        await m.react('🕑');
+        let api = await axios.get(`https://apidl.asepharyana.cloud/api/downloader/igdl?url=${args[0]}`);
+
+        let processedUrls = new Set();
+
+        for (let a of api.data.data) {
+            if (!processedUrls.has(a.url)) {
+                processedUrls.add(a.url);
+
+                // Verificación de si la URL es una imagen o un video
+                if (a.url.includes('jpg') || a.url.includes('png') || a.url.includes('jpeg') || a.url.includes('webp') || a.url.includes('heic') || a.url.includes('tiff') || a.url.includes('bmp')) {
+                    await conn.sendMessage(
+                        m.chat,
+                        { 
+                            image: { url: a.url }, 
+                            caption: '*✔️🍟Downloader instagram.*' 
+                        },
+                        { quoted: m }
+                    );
+                } else {
+                    await conn.sendMessage(
+                        m.chat,
+                        { 
+                            video: { url: a.url }, 
+                            caption: '*✔️🍟Downloader instagram.*' 
+                        },
+                        { quoted: m }
+                    );
+                }
+            }
+        }
+        await m.react('✅'); 
+    } catch (error) {
+        console.log(error);
+        await m.react('❌');
+    }
+};
+
+handler.help = ['ig *<link>*'];
+handler.tags = ['dl'];
+handler.command = /^(ig|igdl|instagram)$/i;
+
+export default handler;
