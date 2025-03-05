058b9a00e10b5de64846e9b1fa73f7c3faedc742
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-03-04 20:21:45 -0400

    Actualizar Main•allfake.js

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -27,50 +27,56 @@ return res.data
 console.log(`Error : ${e}`)
 }}
 
-let who = m.messageStubParameters[0] + '@s.whatsapp.net'
-let user = global.db.data.users[who]
-//let pushname = user ? user.name : await conn.getName(who)
-let pushname = m.pushName || 'Sin nombre'
-
 //creador y otros
-global.creador = 'Wa.me/51923300039'
+global.creador = 'Wa.me/5493876432076'
 global.ofcbot = `${conn.user.jid.split('@')[0]}`
-global.asistencia = 'https://wa.me/message/BTJGZ2PHZGQZO1'
-global.namechannel = 'Ai Otho - Channel'
+global.asistencia = 'https://wa.me/message/O4QPPHZOFDOJI1'
+global.namechannel = '© All Rightd Reserved • GenesisAi'
+global.listo = '✨️ *Aquí tienes ฅ^•ﻌ•^ฅ*'
+global.fotoperfil = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://qu.ax/QGAVS.jpg')
 
-//Reacciones De Comandos.!
+//ids channel
+global.channelid = '120363220939514640@newsletter'
+global.canalIdYL = ["120363220939514640@newsletter", "120363371366801178@newsletter", "120363323775906071@newsletter", "120363343366246815@newsletter"]
+global.canalNombreYL = ["✿ ɢᴇɴᴇꜱɪꜱ • ᴄʜᴀɴɴᴇʟ ᴏꜰᴄ ✿", "ᴄᴀɴᴀʟ • ᴛᴇᴀᴍ ᴄᴏᴅᴇ ᴛɪᴛᴀɴꜱ 🍃", "ᴄᴀɴᴀʟ • ꜰʀᴇᴇ ᴄᴏᴅᴇ ᴛɪᴛᴀɴꜱ 🍧", "ᴄᴀɴᴀʟ • ɪᴢᴜᴍɪ-ᴋᴢx ᴏꜰɪᴄɪᴀʟ ☁️"]
+global.channelRD = await getRandomChannel()
+
+//fechas
+global.d = new Date(new Date + 3600000)
+global.locale = 'es'
+global.dia = d.toLocaleDateString(locale, {weekday: 'long'})
+global.fecha = d.toLocaleDateString('es', {day: 'numeric', month: 'numeric', year: 'numeric'})
+global.mes = d.toLocaleDateString('es', {month: 'long'})
+global.año = d.toLocaleDateString('es', {year: 'numeric'})
+global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true})
+
+//Reacciones y mensaje de espera.
 global.rwait = '🕒'
 global.done = '✅'
 global.error = '✖️'
-
-//Emojis determinado de Ai Otho
-global.emoji = '🌸'
-global.emoji2 = '🍬'
-global.emoji3 = '✨️'
-global.emoji4 = '🍭'
-global.emojis = [emoji, emoji2, emoji3, emoji4].getRandom()
-
-//mensaje en espera
+global.emoji = '🚩'
+global.emoji2 = '🍟'
+global.emoji3 = '🌺'
+global.emoji4 = '🌻'
+global.emoji5 = '✨️'
 global.wait = '🚀 Cargando...'
-global.waitt = '🚀 Cargando...'
-global.waittt = '🚀 Cargando...'
-global.waitttt = '🚀 Cargando...'
+
+global.emojis = [emoji, emoji2, emoji3, emoji4, emoji5].getRandom()
 
 //Enlaces
-var canal = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'  
-var git = 'https://github.com/DanielDiod'  
-var github = 'https://github.com/DanielDiod/Ai-Otho'  
-var panel = 'https://panel.skyultraplus.com'
-var dash = 'https://dash.skyultraplus.com'
-var tienda = 'https://dash.skyultraplus.com/store'
-var status = 'https://estado.skyultraplus.com'
-var discord = 'https://discord.com/invite/T7ksHu7mkz'
-var paypal = 'https://paypal.me/corinplus2024'
-let correo = 'danieldevelop3@gmail.com'
+var canal = 'https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y'
+var canal2 = 'https://whatsapp.com/channel/0029VaGGynJLY6d43krQYR2g'
+var canal3 = 'https://whatsapp.com/channel/0029VawMIXi2P59oaQ2H1j0O'
+var canal4 = 'https://whatsapp.com/channel/0029VaS4zeE72WTyg5et571r'
+var canal5 = 'https://whatsapp.com/channel/0029VavYQDTBlHpcishJCZ0k'
+var github = 'https://github.com/Angelito-OFC/Genesis-AI'  
+var insta = 'https://www.instagram.com/angelito.kzx'
 
-global.redes = [canal, git, github, panel, dash, tienda, status, discord, paypal, correo].getRandom()
+global.redes = [canal, canal2, canal3, canal4, github, insta].getRandom()
 
-global.redeshost = [panel, dash, tienda, status, discord, paypal].getRandom()
+global.channels = [canal, canal5].getRandom()
+
+global.redeshost = [canal, canal2, github, insta].getRandom()
 
 //Imagen
 let category = "imagen"
@@ -79,7 +85,7 @@ const db_ = JSON.parse(fs.readFileSync(db))
 const random = Math.floor(Math.random() * db_.links[category].length)
 const randomlink = db_.links[category][random]
 const response = await fetch(randomlink)
-const rimg = await response.arrayBuffer()
+const rimg = await response.buffer()
 global.icons = rimg
 
 //• ↳ ◜𝑻𝑰𝑬𝑴𝑷𝑶 𝑹𝑷𝑮◞ • ⚔
@@ -87,25 +93,41 @@ var ase = new Date(); var hour = ase.getHours(); switch(hour){ case 0: hour = 'L
 global.saludo = hour;
 
 //tags
-global.nombre = conn.getName(m.sender)
+global.nombre = m.pushName || 'Anónimo'
 global.taguser = '@' + m.sender.split("@s.whatsapp.net")
 var more = String.fromCharCode(8206)
 global.readMore = more.repeat(850)
+global.sticker1 = `☁️Usuario: ${nombre}
+☁️ Creador: Izumi-kzx • ☁️ Fecha: ${fecha}`;
+global.sticker2 = `☁️Bot: ${botname}`
 
 //Fakes
-global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${pushname}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${pushname},;;;\nFN:${pushname},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
+global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `6285600793871-1614953337@g.us` } : {}) }, message: { 'contactMessage': { 'displayName': `${nombre}`, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${nombre},;;;\nFN:${nombre},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`, 'jpegThumbnail': null, thumbnail: null,sendEphemeral: true}}}
 
-// global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: `${packname}`, orderTitle: 'Bang', thumbnail: icons, sellerJid: '0@s.whatsapp.net'}}}
-
-global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363206717994793@newsletter', newsletterName: namechannel, serverMessageId: -1 }
+global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, newsletterName: channelRD.name, serverMessageId: -1 }
 }}, { quoted: m }
 
 global.icono = [ 
-'https://qu.ax/lwEqn.jpg',
-'https://qu.ax/jJpmr.jpg',
-'https://qu.ax/PGFnh.jpg'
+'https://i.ibb.co/0nMY9Y0/file.jpg',
+'https://i.ibb.co/GPXBQXc/file.jpg',
+'https://i.ibb.co/xXzbW1g/file.jpg',
+'https://i.ibb.co/s5cxWDM/file.jpg',
+'https://i.ibb.co/SsGHRCr/file.jpg',
+'https://i.ibb.co/37Vnh5M/file.jpg'
 ].getRandom()
 
-global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363206717994793@newsletter", serverMessageId: 100, newsletterName: namechannel, }, externalAdReply: { showAdAttribution: true, title: namebot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
+global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: channelRD.id, serverMessageId: 100, newsletterName: channelRD.name, }, externalAdReply: { showAdAttribution: true, title: botname, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
 
 export default handler
+
+
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]
+  }
+
+async function getRandomChannel() {
+let randomIndex = Math.floor(Math.random() * canalIdYL.length)
+let id = canalIdYL[randomIndex]
+let name = canalNombreYL[randomIndex]
+return { id, name }
+}         
